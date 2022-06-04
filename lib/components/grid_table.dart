66542a0ef221d8ab3f9416c56e849b4abbc3e6da
yyyoichi <origamiichiro@gmail.com>
2022-06-04 17:38:07 +0900

    gridTable->statefulWidget

--- lib/components/grid_table.dart
+++ lib/components/grid_table.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/components/square.dart';
-
-class GridTable extends StatelessWidget {
+class GridTable extends StatefulWidget {
   final int num;
   const GridTable({Key? key, required this.num}) : super(key: key);
+  @override
+  State<GridTable> createState() => _GridTableState();
+}
+class _GridTableState extends State<GridTable> {
+  //statefullにするところ間違えた。
+  //また使うかもしれないから放置。
+  @override
   Widget build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: num,
+      crossAxisCount: widget.num,
       crossAxisSpacing: 4.0,
       mainAxisSpacing: 4.0,
-      children: List.generate(num * num, (index){
+      children: List.generate(widget.num * widget.num, (index){
         return const Squeare(counter: 0);
       }),
     );
--- lib/table_practice.dart
+++ lib/table_practice.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/components/grid_table.dart';
 class TableApp extends StatelessWidget {
   final int num = 6;
   const TableApp({Key? key}) : super(key: key);
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
