ed25f604019bd6c8f55f0ed85454d81aba56cffa
yyyoichi <origamiichiro@gmail.com>
2022-06-04 21:44:19 +0900

    tableセル数可変

--- lib/components/grid_table.dart
+++ lib/components/grid_table.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/components/square.dart';
-class GridTable extends StatefulWidget {
+// class GridTable extends StatefulWidget {
+//   final int num;
+//   const GridTable({Key? key, required this.num}) : super(key: key);
+//   @override
+//   State<GridTable> createState() => _GridTableState();
+// }
+class GridTable extends StatelessWidget {
   final int num;
   const GridTable({Key? key, required this.num}) : super(key: key);
-  @override
-  State<GridTable> createState() => _GridTableState();
-}
-class _GridTableState extends State<GridTable> {
+
   //statefullにするところ間違えた。
   //また使うかもしれないから放置。
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: widget.num,
+      crossAxisCount: num,
       crossAxisSpacing: 4.0,
       mainAxisSpacing: 4.0,
-      children: List.generate(widget.num * widget.num, (index){
+      children: List.generate(num * num, (index){
         return const Squeare(counter: 0);
       }),
     );
--- lib/table_practice.dart
+++ lib/table_practice.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/grid_table.dart';
 
-class TableApp extends StatelessWidget {
-  final int num = 6;
+class TableApp extends StatefulWidget {
   const TableApp({Key? key}) : super(key: key);
+  @override
+  State<TableApp> createState() => _TableAppState();
+}
+
+class _TableAppState extends State<TableApp> {
+  double _num = 6;
+  void _changeNumOfRow(double n) {
+    setState(() {
+      _num = n;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("make table.")
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(25.0),
-        child: GridTable(num: num),
-        )
-    );
+        appBar: AppBar(title: const Text("make table.")),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(25.0),
+                child: GridTable(num: _num.toInt()),
+              )),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 30.0, horizontal: 25.0),
+                // height: 40.0,
+                child: Slider(
+                  label: '$_num',
+                  min: 5,
+                  max: 10,
+                  value: _num,
+                  divisions: 10,
+                  onChanged: _changeNumOfRow,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
-
