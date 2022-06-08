0e333eff0537d24a35d3da4b0afe7685a5017a58
yyyoichi <origamiichiro@gmail.com>
2022-06-08 12:51:33 +0900

    gridのriverpod化

---
+++ lib/components/riverpod_grid.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_1/components/grid_table.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_application_1/riverpod_table.dart';
+
+class RiverpodGrid extends StatelessWidget {
+  @override 
+  Widget build (BuildContext context) {
+    return Consumer(builder: ((context, ref, child) {
+      final num = ref.watch(riverpodTableProvider.select((state) => state.numOfLines));
+      return GridTable(num: num);
+    }));
+  }
+}
