3f03ecdc8ea3438d08e13b336f35e0da1fd48c37
yyyoichi <origamiichiro@gmail.com>
2022-06-12 14:17:48 +0900

    ディレクトリの整理

--- lib/components/grid_table.dart
+++ lib/components/grid_table.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_application_1/components/square.dart';
-// class GridTable extends StatefulWidget {
-//   final int num;
-//   const GridTable({Key? key, required this.num}) : super(key: key);
-//   @override
-//   State<GridTable> createState() => _GridTableState();
-// }
 class GridTable extends StatelessWidget {
-  final int num;
   const GridTable({Key? key, required this.num}) : super(key: key);
-
-  //statefullにするところ間違えた。
-  //また使うかもしれないから放置。
+  final int num;
   @override
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: num,
       crossAxisSpacing: 4.0,
       mainAxisSpacing: 4.0,
-      children: List.generate(num * num, (index){
-        return const Squeare(counter: 0);
+      children: List.generate(num * num, (index) {
+        return Container(
+          color: const Color(0xFF000FF0),
+          width: 100.0,
+          height: 100.0,
+        );
       }),
     );
   }
