3ff4cbe0e8785555f938fb8bbde264dd80a46ca8
yyyoichi <origamiichiro@gmail.com>
2022-07-13 21:21:05 +0900

    gamestart flame

--- lib/components/go_to_game.dart
+++ lib/components/go_to_game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/state/game_stage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class GoToGame extends StatelessWidget {
@@ -6,13 +7,13 @@ class GoToGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: ((context, ref, child) {
-      // final notifier = ref.watch(riverpodTableProvider.notifier);
+      final notifier = ref.read(gameStageProvider.notifier);
       return Container(
           margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
           child: TextButton(
               child: const Text("next =>"),
               onPressed: () {
-                Navigator.pushNamed(context, "/game");
+                notifier.start();
               }));
     }));
   }
--- lib/game_app.dart
+++ lib/game_app.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/loading.dart';
 import 'package:flutter_application_1/pages/game_setting.dart';
+import 'package:flutter_application_1/state/game_stage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class GameApp extends StatelessWidget {
   const GameApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Consumer(builder: (context, ref, child){
+      final stage = ref.watch(gameStageProvider);
+      return Scaffold(
         appBar: AppBar(title: const Text("Reverpod.")),
         body: Center(
-            child: Stack(
-          children: const [GameSetting()],
-        )));
+            child: (stage=="setting") ? const GameSetting() : const Loading()
+        ));
+    }); 
   }
 }
