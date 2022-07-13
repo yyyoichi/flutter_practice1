6226d68a328074ae660d9dc39e817e8acc268b65
yyyoichi <origamiichiro@gmail.com>
2022-07-13 20:21:59 +0900

    add start gamestate

--- lib/state/gaming.dart
+++ lib/state/gaming.dart
@@ -3,17 +3,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 @immutable
 class Gaming {
+  final bool isGaming;
   final bool isA;
   final bool isLoading;
   final List<History> histories;
 
   const Gaming(
-      {this.isA = true,
+      {this.isGaming = false,
+      this.isA = true,
       this.isLoading = false,
       this.histories = const <History>[]});
 
-  Gaming copyWith({bool? isA, bool? isLoading, List<History>? histories}) {
+  Gaming copyWith(
+      {bool? isGaming, bool? isA, bool? isLoading, List<History>? histories}) {
     return Gaming(
+        isGaming: isGaming ?? this.isGaming,
         isA: isA ?? this.isA,
         isLoading: isLoading ?? this.isLoading,
         histories: histories ?? this.histories);
@@ -29,11 +33,11 @@ class History {
 
 class GameNotifier extends StateNotifier<Gaming> {
   GameNotifier() : super(const Gaming());
+  void startGame() {
+    state.copyWith(isGaming: true);
+  }
   void onChangePlayer() {
-    state.copyWith(
-      isA: !state.isA,
-      isLoading: !state.isLoading
-    );
+    state.copyWith(isA: !state.isA, isLoading: !state.isLoading);
   }
 }
 
