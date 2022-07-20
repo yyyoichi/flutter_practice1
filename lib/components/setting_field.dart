f009f9dc6444feaba50f2fb355ade072b4c06171
yyyoichi <origamiichiro@gmail.com>
2022-07-20 20:57:10 +0900

    fix setting error

--- lib/components/setting_field.dart
+++ lib/components/setting_field.dart
@@ -8,17 +8,21 @@ class SettingField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: ((context, ref, child) {
-      final num = ref.watch(riverpodTableProvider.select((state) => state.numOfLines));
-      final posIslands = ref.watch(riverpodTableProvider.select((state) => state.positionsOfIsland));
+      final num =
+          ref.watch(riverpodTableProvider.select((state) => state.numOfLines));
+      final posIslands = ref.watch(
+          riverpodTableProvider.select((state) => state.positionsOfIsland));
       const sea = Color.fromARGB(255, 131, 134, 172);
       const island = Color.fromARGB(255, 28, 54, 32);
       return GridView.count(
         crossAxisCount: num,
         crossAxisSpacing: 3.0,
         mainAxisSpacing: 3.0,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
         children: List.generate(num * num, (index) {
           return Container(
-            color: posIslands.contains(index) ? island: sea,
+            color: posIslands.contains(index) ? island : sea,
             width: 100.0,
             height: 100.0,
           );
